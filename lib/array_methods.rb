8fabf11a1e058b3ef734f77cab157a9ce099eafb
Richell Bonfield <richell.bonfield12@gmail.com>
2019-08-22 12:34:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,12 @@
 def find_element_index(array, value_to_find)
-  array.length.times { |index|
+  length = array.length
+  
+  length.times do |index|
     if value_to_find = array[index]
       return array[index]
     end
-  }
+      array[index]
+  end
 end
 
 def find_max_value(array)
